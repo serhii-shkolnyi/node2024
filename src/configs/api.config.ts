3a089960b31a6f08e8efd1671b5e4924fefe8392
serhii-shkolnyi <s.shkolnyi.work@gmail.com>
2024-01-22 17:34:09 +0200

    auth add refresh token

--- src/configs/api.config.ts
+++ src/configs/api.config.ts
@@ -9,6 +9,9 @@ export const apiConfig = {
 
   SALT_ROUNDS: process.env.SALT_ROUNDS || 5,
 
+  EXPIRES_IN_ACCESS: process.env.EXPIRES_IN_ACCESS,
+  EXPIRES_IN_REFRESH: process.env.EXPIRES_IN_REFRESH,
+
   JWT_ACCESS_SECRET: process.env.JWT_ACCESS_SECRET,
   JWT_REFRESH_SECRET: process.env.JWT_REFRESH_SECRET,
 };
